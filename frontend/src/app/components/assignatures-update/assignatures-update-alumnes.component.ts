12b15c5de60ba266375679949b26157bbc779f25
amoya <amoya@amoya>
2017-11-30 15:35:37 +0100

    18-assignatures-update

--- frontend/src/app/components/assignatures-update/assignatures-update-alumnes.component.ts
+++ frontend/src/app/components/assignatures-update/assignatures-update-alumnes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AssignaturesUpdateComponent } from './assignatures-update.component';
 
 import { AssignaturesDataService } from '../../services/assignatures-data.service';
-
+import { AlumnesService } from '../../services/alumnes.service';
 
 @Component({
 	selector: 'app-assignatures-update-alumnes',
@@ -12,18 +12,38 @@ import { AssignaturesDataService } from '../../services/assignatures-data.servic
 
 export class AssignaturesUpdateAlumnesComponent implements OnDestroy {
 
-	assignatura: any;
-	alumnes: any;
-	title: string = "Alumnes";
+	private sub: any;
+	private assignatura: any;
+	private alumnes: any[] = [];
+	private title: string = "Alumnes";
 
 	constructor(
+		private _alumnes: AlumnesService,
 		private _assignaturesData: AssignaturesDataService) {
 			this.assignatura = this._assignaturesData.getAssignatura();
-			this.alumnes = this._assignaturesData.get_distinct_alumnes();
+			// this.alumnes = this._assignaturesData.get_distinct_alumnes();
+
+			this.sub = this._alumnes.list().subscribe(alumnes => {
+				for (let alumne of alumnes) {
+					this._alumnes.get(alumne.id).subscribe(alumneData => {
+						for (let assignatura of alumneData.assignatures) {
+							if (assignatura == this.assignatura.id) {
+								this.alumnes.push(alumneData);
+								console.log(this.alumnes);
+							}
+						}
+					})
+				}
+			})
+	};
+
+	getAlumnes(assignaturaId: string) {
+
 	};
 
 	ngOnDestroy() {
 		this._assignaturesData.passAssignatura(this.assignatura);
+		this.sub.unsubscribe();
 	};
 
 };
